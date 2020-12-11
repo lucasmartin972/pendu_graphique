c3ec9cc1f6e4b574aac8dbd67c99769f190637f8
John Doelucas <lucas.martin972@gmail.com>
2020-12-11 11:45:35 +0100

    version 0.2

--- pendu_graphique.py
+++ pendu_graphique.py
@@ -10,17 +10,32 @@ Created on Thu Dec 10 22:03:55 2020
 #Martin Lucas
 #To Do: 
     
-from tkinter import Tk, Label, Button , Frame
+    
+#Création fenetre principale
+from tkinter import *
+from fonctions import *
 mw = Tk()
 mw.title('Jeu du Pendu')
-mw['bg']=''
+mw['bg']='#3D4042'
 mw.geometry('640x360+400+200')
-labelHello = Label(mw, text = "petite partie de pendu?!!", fg = 'blue')
-labelHello.pack()
 
 
-FrameAffichageMot = Frame(mw,relief='groove')
-FrameAffichageMot.pack(side='top',padx=10,pady=10)
+
+
+#Création Frame interface utilisateur
+FrameAffichageMot =Frame(mw,relief='groove')
+FrameAffichageMot.pack(side='left',padx=10,pady=10)
+
+
+#input de l'utilisateur
+LettreEntre = StringVar()
+Champ=Entry(FrameAffichageMot, textvariable='LettreEntre')
+Champ.focus_set(); #on met le focus des entrées utilisaterur sur le champ
+Champ.pack(side='bottom',padx=5,pady=5)
+
+
+
+
 
 
 
@@ -41,12 +56,50 @@ def NouvellePartie():
 
     #le jeu commence
     jeu(mots_pendu,score_max)
-
-
-Label(FrameAffichageMot, text = 'iugdziugdhz').pack(padx=10, pady=10)
-
-BoutonProposer = Button(FrameAffichageMot, text ='Proposer', command=NouvellePartie)
-BoutonProposer.pack(side='right',padx=1,pady=1)
     
+    
+    
+  
+    
+    
+
+
+Label(FrameAffichageMot, text = 'Proposez une lettre').pack(padx=10, pady=10)
+
+
+
+
+
+BoutonNouvellePartie = Button(mw, text ='Nouvelle Partie?', command=NouvellePartie)
+BoutonNouvellePartie.pack(side='top',padx=1,pady=1)
+
+
+
+
+
+    
+#création d'un canvas
+Canevas= Canvas(mw,width=300,height=300, bg ='#3D4042')
+photo=PhotoImage(file="bonhomme1.gif")
+Canevas.create_image(0,0,anchor=NW,image=photo)
+Canevas.pack()                                             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 mw.mainloop()
